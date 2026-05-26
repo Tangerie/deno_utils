fd66138c259923cbbaa312ad9ee1b7309c219acd
Tangerie <josh.cavill01@gmail.com>
2026-05-26 10:14:08 +1000

    added opts to trange

--- tqdm/misc.ts
+++ tqdm/misc.ts
@@ -1,4 +1,4 @@
-import { tqdm } from "./tqdm.ts";
+import { tqdm, type TqdmOptions } from "./tqdm.ts";
 
 async function* range(start: number, end : number): AsyncIterableIterator<number> {
     for (let i = start; i < end; i++) {
@@ -6,6 +6,6 @@ async function* range(start: number, end : number): AsyncIterableIterator<number
     }
 }
 
-export function trange(start : number, end : number): AsyncIterableIterator<number> {
-    return tqdm(range(start, end), { size: end - start })
+export function trange(start : number, end : number, opts ?: Omit<TqdmOptions, "size">): AsyncIterableIterator<number> {
+    return tqdm(range(start, end), { size: end - start, ...opts })
 }
